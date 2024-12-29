1f1e1d26badda4f90840557cc9f2530977e9cf29
Vipul Tyagi <88539287+iamvipultyagi@users.noreply.github.com>
2024-12-29 16:26:09 +0000

    First commit

--- Q13.js
+++ Q13.js
@@ -1,1 +1,15 @@
-fujldn
+let score=[30,40,32,78,94,56];
+let count=0;
+for(let i=0;i<score.length;i++){
+    if(score[i]<40){
+        score[i]+=20;
+    }
+    if(score[i]>90){
+        score[i]=90;
+    }
+    if(score[i]>=50){
+        count++;
+    }
+}
+console.log("Final marks",score)
+console.log("Passed student",count)
